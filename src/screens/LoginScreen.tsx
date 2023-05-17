f720957e3498f9f233d973d6d3f73e9e4904ce66
aalleexx98 <alex92garcia2@gmail.com>
2023-05-17 19:41:00 +0200

    add icons

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 export const LoginScreen = () => {
     return (
         <View>
             <Text>LoginScreen</Text>
+            <Icon
+                name="at-circle-outline"
+                size={ 50 }
+                color="grey"
+            />
         </View>
     )
 }
