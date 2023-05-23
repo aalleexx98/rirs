1769cab81d9c4a491fe363150372bce32ca35843
aalleexx98 <alex92garcia2@gmail.com>
2023-05-23 17:57:49 +0200

    profile picture component

---
+++ src/components/home/ProfilePicture.tsx
@@ -0,0 +1,98 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { Alert, View } from 'react-native'
+import { launchImageLibrary } from 'react-native-image-picker';
+import { AuthContext } from '../../context/authContext/authContext';
+import storage from '@react-native-firebase/storage';
+import auth from '@react-native-firebase/auth';
+import { Image } from '@rneui/base';
+import { FadeInImage } from '../../components/global/FadeInImage';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+export const ProfilePicture = () => {
+
+    const { user, uid } = useContext(AuthContext);
+
+    const [tempUri, setTempUri] = useState<string>()
+
+    useEffect(() => {
+        if (user?.photoURL) {
+            setTempUri(user.photoURL);
+        }
+    }, [])
+
+    const takePhotoFromGallery = async () => {
+        launchImageLibrary({
+            mediaType: 'photo',
+            quality: 0.5
+        }, async (resp) => {
+            if (resp.didCancel) return;
+            if (resp.assets) {
+                if (!resp.assets[0].uri) return;
+
+                const imageURL = await uploadImage(resp.assets[0].uri);
+                if (imageURL) {
+                    setTempUri(imageURL);
+                    updateProfilePhotoURL(imageURL);
+                }
+            }
+        })
+    }
+
+    const uploadImage = async (uri: string) => {
+        try {
+            const fileName = uid;
+            if (fileName) {
+                const reference = storage().ref(fileName);
+
+                await reference.putFile(uri);
+                const url = await reference.getDownloadURL();
+
+                return url;
+            }
+        } catch (error: any) {
+            Alert.alert('Error', 'Error al subir la imagen');
+        }
+    }
+
+    const updateProfilePhotoURL = async (photoURL: string) => {
+        const user = auth().currentUser;
+
+        try {
+            if (user) {
+                await user.updateProfile({
+                    photoURL: photoURL
+                });
+            } else {
+                console.log('El usuario no está autenticado');
+            }
+        } catch (error) {
+            Alert.alert('Error', 'Error al actualizar la URL de foto');
+        }
+    };
+
+    return (
+        <>
+            { tempUri ? (
+                <View style={ { borderRadius: 200, overflow: 'hidden', height: 200, width: 200 } }>
+                    <TouchableOpacity onPress={ takePhotoFromGallery }>
+                        <FadeInImage
+                            key={ tempUri }
+                            uri={ tempUri }
+                            style={ { width: 200, height: 200, resizeMode: 'cover' } }
+                        />
+
+                    </TouchableOpacity>
+                </View>
+            ) : (
+                <View style={ { borderRadius: 200, overflow: 'hidden', height: 200, width: 200 } }>
+                    <TouchableOpacity onPress={ takePhotoFromGallery }>
+                        <Image
+                            source={ require('../../assets/blank-profile.png') }
+                            style={ { width: 200, height: 200, resizeMode: 'cover' } }
+                        />
+                    </TouchableOpacity>
+                </View>
+            ) }
+        </>
+    )
+}
--- src/screens/HomeScreens/HomeScreen.tsx
+++ src/screens/HomeScreens/HomeScreen.tsx
@@ -1,78 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Alert, Button, Text, View } from 'react-native'
-import { launchImageLibrary } from 'react-native-image-picker';
+import React, { useContext } from 'react'
+import { Button, Text, View } from 'react-native'
 import { AuthContext } from '../../context/authContext/authContext';
-import storage from '@react-native-firebase/storage';
-import auth from '@react-native-firebase/auth';
-import { Image } from '@rneui/base';
-import { FadeInImage } from '../../components/global/FadeInImage';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { ProfilePicture } from '../../components/home/ProfilePicture';
 
 
 
 export const HomeScreen = () => {
-    const { user, logOut, uid } = useContext(AuthContext);
-
-    const [tempUri, setTempUri] = useState<string>()
-
-    useEffect(() => {
-        if (user?.photoURL) {
-            setTempUri(user.photoURL);
-        }
-    }, [])
-
-    const takePhotoFromGallery = async () => {
-        launchImageLibrary({
-            mediaType: 'photo',
-            quality: 0.5
-        }, async (resp) => {
-            if (resp.didCancel) return;
-            if (resp.assets) {
-                if (!resp.assets[0].uri) return;
-
-                const imageURL = await uploadImage(resp.assets[0].uri);
-                if (imageURL) {
-                    setTempUri(imageURL);
-                    updateProfilePhotoURL(imageURL);
-                }
-            }
-        })
-    }
-
-    const uploadImage = async (uri: string) => {
-        try {
-            const fileName = uid;
-            if (fileName) {
-                const reference = storage().ref(fileName);
-
-                await reference.putFile(uri);
-                const url = await reference.getDownloadURL();
-
-                return url;
-            }
-        } catch (error: any) {
-            Alert.alert('Error', 'Error al subir la imagen');
-        }
-    }
-
-    const updateProfilePhotoURL = async (photoURL: string) => {
-        const user = auth().currentUser;
-
-        try {
-            if (user) {
-                await user.updateProfile({
-                    photoURL: photoURL
-                });
-                if (uid) storage().ref(uid).delete
-
-            } else {
-                console.log('El usuario no está autenticado');
-            }
-        } catch (error) {
-            Alert.alert('Error', 'Error al actualizar la URL de foto');
-        }
-    };
+    const { user, logOut } = useContext(AuthContext);
 
     return (
         <View>
@@ -86,28 +20,7 @@ export const HomeScreen = () => {
                 onPress={ logOut }
             />
 
-            {/* CUANDO CREE EL APARTADO DE PERFIL PONER LOS ESTILOS POR SEPARADOS YA QUE SE DUPLICA */ }
-            { tempUri ? (
-                <View style={ { borderRadius: 200, overflow: 'hidden', height: 200, width: 200 } }>
-                    <TouchableOpacity onPress={ takePhotoFromGallery }>
-                        <FadeInImage
-                            key={ tempUri }
-                            uri={ tempUri }
-                            style={ { width: 200, height: 200, resizeMode: 'cover' } }
-                        />
-
-                    </TouchableOpacity>
-                </View>
-            ) : (
-                <View style={ { borderRadius: 200, overflow: 'hidden', height: 200, width: 200 } }>
-                    <TouchableOpacity onPress={ takePhotoFromGallery }>
-                        <Image
-                            source={ require('../../assets/blank-profile.png') }
-                            style={ { width: 200, height: 200, resizeMode: 'cover' } }
-                        />
-                    </TouchableOpacity>
-                </View>
-            ) }
+            <ProfilePicture />
 
 
         </View>
