7faf2b89066d7660d135a57fc172415fb2fde8f0
aalleexx98 <alex92garcia2@gmail.com>
2023-05-26 19:44:08 +0200

    add some info in details Screen

--- src/screens/ExerciceDetailsScreen.tsx
+++ src/screens/ExerciceDetailsScreen.tsx
@@ -1,6 +1,6 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import React, { useContext, useEffect, useState } from 'react'
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Dimensions, FlatList, StyleSheet, Text, TouchableOpacity, View, Linking } from 'react-native'
 import { RootStackParams } from '../routes/ExerciceStack'
 import firestore from '@react-native-firebase/firestore';
 import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
@@ -26,22 +26,21 @@ export const ExerciceDetailsScreen = ({ navigation, route }: Props) => {
             if (documentSnapshot.exists) {
                 const documentData = documentSnapshot.data();
                 setExercice(documentData);
-                console.log(exercice);
+                console.log(documentData);
             }
         } catch (error) {
             console.log('Error al obtener el documento:', error);
         }
     }
 
+    const handlePress = () => {
+        const youtubeURL = exercice?.video_link;
+        Linking.openURL(youtubeURL);
+    };
+
     useEffect(() => {
         getFullExercice();
     }, [])
-
-    // React.useLayoutEffect(() => {
-    //     navigation.setOptions({
-    //         title: exercice?.name,
-    //     });
-    // }, [navigation]);
 
     return (
         <>
@@ -63,12 +62,54 @@ export const ExerciceDetailsScreen = ({ navigation, route }: Props) => {
             </TouchableOpacity>
             { exercice ?
                 (
-                    <View style={ { ...styles.containerImage, width: windowWith } }>
-                        <FadeInImage
-                            uri={ exercice.image }
-                            style={ { height: 250, width: 250 } }
-                        />
-                    </View>
+                    <>
+                        <View style={ { ...styles.containerImage, width: windowWith } }>
+                            <FadeInImage
+                                uri={ exercice.image }
+                                style={ { height: 250, width: 250 } }
+                            />
+                        </View>
+                        <View style={ { ...styles.containerDetails } }>
+                            <Text style={ { ...styles.title, color: colors.text } }>{ exercice.name }</Text>
+
+                            <TouchableOpacity onPress={ handlePress } style={ { ...styles.botonYT, borderColor: colors.text } }>
+                                <Text style={ { color: colors.text } }>Ver video en YouTube</Text>
+                            </TouchableOpacity>
+
+                            <View style={ { rowGap: 5 } }>
+                                <Text style={ { ...styles.sectionTitle, color: colors.text } }>Ejecucción.</Text>
+                                <FlatList
+                                    data={ exercice.execution }
+                                    renderItem={ ({ item }) => <Text style={ { color: colors.text } }>{ item }</Text> }
+                                    keyExtractor={ (item, index) => index.toString() }
+                                />
+                            </View>
+
+                            <View style={ { rowGap: 5 } }>
+                                <Text style={ { ...styles.sectionTitle, color: colors.text } }>Tips.</Text>
+                                <FlatList
+                                    data={ exercice.tips }
+                                    renderItem={ ({ item }) => <Text style={ { color: colors.text } }> - { item }</Text> }
+                                    keyExtractor={ (item, index) => index.toString() }
+                                />
+                            </View>
+
+                            <View style={ { rowGap: 5 } }>
+                                <Text style={ { ...styles.sectionTitle, color: colors.text } }>Músculo Principal:</Text>
+                                <Text style={ { color: colors.text, textTransform: 'capitalize' } }>{ exercice.primaryMuscle }</Text>
+                            </View>
+
+                            <View style={ { rowGap: 5 } }>
+                                <Text style={ { ...styles.sectionTitle, color: colors.text } }>Músculos Secundarios:</Text>
+                                <FlatList
+                                    data={ exercice.tips }
+                                    renderItem={ ({ item }) => <Text style={ { color: colors.text } }> Poner musculo, musculo</Text> }
+                                    keyExtractor={ (item, index) => index.toString() }
+                                />
+                            </View>
+
+                        </View>
+                    </>
                 )
                 :
                 (
@@ -93,7 +134,23 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    image: {
-
+    containerDetails: {
+        flex: 1,
+        padding: 15,
+        rowGap: 20,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '600'
+    },
+    sectionTitle: {
+        fontSize: 18,
+        fontWeight: '600'
+    },
+    botonYT: {
+        alignItems: 'center',
+        borderWidth: 1,
+        padding: 4,
+        borderRadius: 10,
     }
 });
